84e2c17d018d716c761d4ea1ed9d8955117a20fa
Augustin <Augustin@LAPTOP-BFMSOTN6>
2020-03-26 07:47:11 +0530

    added m2o,o2m,m2m and float data type fields

--- College/addons/clg_base/models/student.py
+++ College/addons/clg_base/models/student.py
@@ -9,11 +9,16 @@ class Student(models.Model):
     last_name = fields.Char(string="Last Name")
     gender = fields.Selection([('male','Male'),('female','Female'),('other','Other')],string="Gender")
     dob = fields.Date(string="Date of Birth")
+    height = fields.Float(string="height")
     age = fields.Integer(string="Age")
     admitted_date = fields.Datetime(string="Admitted Date and time")
     tc_12 = fields.Binary(string="12th TC")
     remarks = fields.Text(string="Remarks")
     description = fields.Html(string="Description")
+    department_id = fields.Many2one('clg.department',string="Department",ondelete="set null")
+    language_ids = fields.Many2many('clg.language',string="Languages known")
+    education_ids = fields.One2many('stud.education','student_id',string="Education Details",help="Previous Education Details")
+    attachment_ids = fields.Many2many('ir.attachment',string="Attachment")
     
 class Department(models.Model):
     _name = "clg.department"
@@ -23,5 +28,19 @@ class Department(models.Model):
     code = fields.Char(string="Code")
     abbreviation = fields.Char(string="Abbreviation")
     
+class Languages(models.Model):
+    _name = 'clg.language'
+    _description = "Language"
+    
+    name = fields.Char(string="Name")
+    
+class Education(models.Model):
+    _name = 'stud.education'
+    _description = "Education"
+    
+    student_id = fields.Many2one('clg.student')
+    level = fields.Char(string="Level")
+    program = fields.Char(string="Progarm")
+    mode = fields.Selection([('regular','Regular'),('private','Private'),('not_applicable','Not Applicable')],string="Mode")
     
     
